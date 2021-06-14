2b20a5b80ef73d038640aa2f2bd5b57606f7d895
Corso <yisus.corso@gmail.com>
2021-06-14 19:46:10 +0000

    final

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,33 +1,28 @@
 import React, { useState } from "react";
 
-
 //create your first component
 export function Home() {
+	const [selectedColor, setSelectedColor] = useState("yellow");
 
-    const [selectedColor, setSelectedColor] = useState("yellow");
+	return (
+		<div className="traffic-light">
+			<div
+				onClick={() => setSelectedColor("red")}
+				className={
+					"light red" + (selectedColor === "red" ? " glow" : "")
+				}></div>
 
+			<div
+				onClick={() => setSelectedColor("yellow")}
+				className={
+					"light yellow" + (selectedColor === "yellow" ? " glow" : "")
+				}></div>
 
-    return (
-        <div className="traffic-light">
-
-            <div
-                onClick={() => setSelectedColor("red")}
-                className={"light red" + ((selectedColor === "red") ? " glow" : "")}>
-
-            </div>
-
-            <div
-                onClick={() => setSelectedColor("yellow")}
-                className={"light yellow" + ((selectedColor === "yellow") ? " glow" : "")}>
-
-            </div>
-            
-            <div 
-                onClick={() => setSelectedColor("green")} 
-                className={"light green" + ((selectedColor === "green") ? " glow" : "")}>
-
-            </div>
-        
-        </div>
-    );
+			<div
+				onClick={() => setSelectedColor("green")}
+				className={
+					"light green" + (selectedColor === "green" ? " glow" : "")
+				}></div>
+		</div>
+	);
 }
